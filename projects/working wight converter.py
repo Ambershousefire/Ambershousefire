7739d55a3319e494a23b017202054d2100830c41
Ambershousefire <amber85077427@gmail.com>
2024-08-29 22:35:57 +0100

    modified:   projects/working wight converter.py

--- projects/working wight converter.py
+++ projects/working wight converter.py
@@ -1,15 +1,24 @@
 run=input("this is a metric imperul converter, whtich do you wahnat to contert, distance or whight: ")
-if run.capitalize().startswith("D"):
-    y=float(input("disance in numers:"))
-    kg =input("meaters or feet: ")
+
+def new_func(kg, x):
+    if kg.upper().startswith("L"):
+        output = str(x * 0.453592)+"kg"
+    elif kg.upper().startswith("K"):
+        output = str(x * 2.20462)+"lb"
+    print( output)
+def new_func1(x, kg):
     if kg.upper().startswith("F"):
-        print( y * 0.3048,"m")
+        output = str(x * 0.3048)+"m"
     elif kg.upper().startswith("M"):
-        print( y * 3.2808,"ft")
+        output = str(x * 3.2808)+"ft"
+    print(output)
+
+if run.capitalize().startswith("D"):
+    x=float(input("disance in numers:"))
+    kg =input("meaters or feet: ")
+    new_func1(x, kg)
+
 elif not run.capitalize().startswith("D"):
     x =float(input("wight in numers:"))
     kg =input("Kg or Lb: ")
-    if kg.upper().startswith("L"):
-        print( x * 0.453592,"kg")
-    elif kg.upper().startswith("K"):
-        print( x * 2.20462,"lb")
+    new_func(kg, x)
