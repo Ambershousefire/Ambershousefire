fefe87b30a1cbb4d7a69e3d3bb1cae952826e48a
Ambershousefire <amber85077427@gmail.com>
2024-08-31 11:53:48 +0100

    modified:   projects/rock battle.py

--- projects/rock battle.py
+++ projects/rock battle.py
@@ -1,3 +1,4 @@
+
 import random
 win="   You Won :3"
 loss="  You lost >:3"
@@ -9,37 +10,49 @@ run=input("do you whant to play rock paper sisors(yes or no): ")
 if run.capitalize().startswith("Y"):
     run=True
 elif not run.capitalize().startswith("Y"):
+    
         print("aww i whanted to play")
+        
 if run==True:
     npc=[1,2,3] #rock is 1 paper is 2 sisors are 3
     b=0
+    
     while b<3:
          b=b+1
+         
          random.shuffle(npc)
          x=int(input("ok to paly rock(1) paper(2) sisors(3) you will say what you whant to play and we will see who wins: "))    
          z=npc[0]
+         
          if z==x:
             d=+1
             print(draw)
+            
          if z==(1):#R
             if x==2:#P
                 w=+1
                 print (win)
+                
             elif x==3:#S
                     l=+1
                     print (loss)  
+                    
          elif z==(2):#P
             if x==3:#S
                 w=+1
                 print (win)
+                
             elif x==1:#R
                     l=+1
                     print(loss)
+                    
          elif z==(3):#S
             if x==1:#R
                 w=+1
                 print(win)
+                
             elif x==2:#P
                     l=+1
                     print(loss)
+                    
     print("wins",w,"losses",l,"draws",d)
