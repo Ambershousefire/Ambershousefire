824f29254ed20b2fba5f19cbc15eb2c70f66488f
Ambershousefire <amber85077427@gmail.com>
2024-09-09 20:31:36 +0100

    modified:   projects/snake.py

--- projects/snake.py
+++ projects/snake.py
@@ -5,6 +5,8 @@ import pygame
 global screen
 x=450
 y=220
+alpha=0
+bata=0
 pos=((x,y))
 black=(0,0,0)
 white=(255,255,255)
@@ -19,6 +21,10 @@ xpos=r.randint(1,89)
 xpos=xpos*10
 ypos=r.randint(1,89)
 ypos=ypos*10
+def snake(screen, x, y, alpha, bata, black, white):
+    pygame.draw.circle(screen,black,(x,y),(10))
+    pygame.draw.circle(screen,white,(x+alpha,y+bata),(10))
+
 while running:
     pygame.display.set_caption("snail""score:",str(score))
     if score==scorecount:
@@ -33,21 +39,39 @@ while running:
     
         
     screen.fill(green)
-    pygame.draw.circle(screen,red,((xpos),(ypos)),(10))
+    pygame.draw.circle(screen,red,((xpos),(ypos)),(5))
     pygame.time.delay(delay)
-    pygame.draw.circle(screen,black,(x,y),(10))
+    snake(screen, x, y, alpha, bata, black, white)
+    
     
     for event in pygame.event.get():
+        if y>440:
+            y=10
+        elif y<10:
+            y=440
+        if x>890:
+            x=10
+        elif x<10:
+            x=890
+
         if event.type == pygame.QUIT: 
              running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
+                bata=+10
+                alpha=0
                 y=y-10
             elif event.key == pygame.K_DOWN:
+                bata=-10
+                alpha=0
                 y=y+10
             elif event.key == pygame.K_LEFT:
+                alpha=+10
+                bata=0
                 x=x-10
             elif event.key == pygame.K_RIGHT:
+                alpha=-10
+                bata=0
                 x=x+10
             elif event.key == pygame.K_SPACE:
                 score= score+1
@@ -58,7 +82,7 @@ while running:
                 
     #debug
 
-    print("player:",x,y,"apple:",xpos,ypos,"score:",score)
+    print("player:",x,y,alpha,bata,"apple:",xpos,ypos,"score:",score)
             
              
 
