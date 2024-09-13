6568a42e631c12ee746fb2ea3dde825a7ff98547
Ambershousefire <amber85077427@gmail.com>
2024-09-13 13:58:22 +0100

    modified:   projects/enger graph.py

--- projects/enger graph.py
+++ projects/enger graph.py
@@ -2,9 +2,20 @@
 import pygame
 print("this is a graphing tool for alloy Graphs")
 print("the max temp on this graph is 500 so your max temp must be less than 500, so insure your numbers are all hole numbers under 500")
-start1=int(input("what is the satring point for liqufaction: "))
-count=int(input("how many points: "))
-
+v=(input("is the highest temp over 500 (y/n)"))
+if v.capitalize().startswith("Y"):
+    o=2
+else:
+    o=1
+count=10
+sort=(input("are you using this grap for eutetic alloys (y/n)"))
+sorttype=False
+if sort.capitalize().startswith("Y"):
+    start1=int(input("what is the end point for solifcation: "))
+    sorttype=True
+if sorttype==False:
+    start1=int(input("what is the sart point for solidfcation: "))
+    start2=int(input("what is the end point for solifcation: "))
 l=1000
 w=500
 screen=pygame.display.set_mode((l,w))
@@ -20,6 +31,8 @@ xgrid=0
 ygrid=0
 f=0
 z=0
+ef=0
+ez=0
 num=0
 temp=0
 xpos=100
@@ -29,33 +42,70 @@ run1=True
 
 screen.fill(white)
 while running==True:
-    while z<count:
-        if run1:
-            mintemp=int(input("Min temperture: "))
+    if sorttype:
+        while z<count:
+            if run1:
+                mintemp=int(input("end of solifcation: "))
+                mintemp=(w-mintemp)
+                start1=(w-start1)
+                run1=False
+            maxtemp=int(input("sart of solifcation: "))
+
+            maxtemp=(w-maxtemp)
+
+            if z==0:
+                f=maxtemp
+        
+            pygame.draw.line(screen,black,(x2pos,mintemp),(xpos,mintemp),(3))
+            if z==0:
+                pygame.draw.line(screen,black,(x2pos,start1),(xpos,maxtemp),(3))
+                xpos+=100
+                x2pos+=100
+            else:
+                pygame.draw.line(screen,black,(xpos,maxtemp),(xpos,maxtemp),(3))
+                pygame.draw.line(screen,black,(x2pos,f),(xpos,maxtemp),(3))
+                
+                f=maxtemp
+                xpos+=100
+                x2pos+=100
+            z=z+1
+    else:
+        while z<count:
+            mintemp=int(input("end of soilifaction: "))
+            maxtemp=int(input("sart of solifcation: "))
+            mintemp=mintemp/o
+            maxtemp=maxtemp/o
             mintemp=(w-mintemp)
+            maxtemp=(w-maxtemp)
+            start1=(start1/o)
+            start2=(start2/o)
             start1=(w-start1)
-            run1=False
-        maxtemp=int(input("Max temperture: "))
-
-        maxtemp=(w-maxtemp)
-
-        if z==0:
-            f=maxtemp
-    
-        pygame.draw.line(screen,black,(x2pos,mintemp),(xpos,mintemp),(3))
-        if z==0:
-            pygame.draw.line(screen,black,(x2pos,start1),(xpos,maxtemp),(3))
-            xpos+=100
-            x2pos+=100
-        else:
-            pygame.draw.line(screen,black,(xpos,maxtemp),(xpos,maxtemp),(3))
-            pygame.draw.line(screen,black,(x2pos,f),(xpos,maxtemp),(3))
+            start2=(w-start2)
             
-            f=maxtemp
-            xpos+=100
-            x2pos+=100
-        z=z+1
-        pygame.display.flip()
+
+            if z==0:
+                ef=maxtemp
+                eg=mintemp
+        
+        
+            if z==0:
+                pygame.draw.line(screen,black,(x2pos,start2),(xpos,mintemp),(3))
+                pygame.draw.line(screen,black,(x2pos,start1),(xpos,maxtemp),(3))
+                xpos+=100
+                x2pos+=100
+            else:
+                pygame.draw.line(screen,black,(xpos,mintemp),(xpos,mintemp),(3))
+                pygame.draw.line(screen,black,(x2pos,eg),(xpos,mintemp),(3))
+                pygame.draw.line(screen,black,(xpos,maxtemp),(xpos,maxtemp),(3))
+                pygame.draw.line(screen,black,(x2pos,ef),(xpos,maxtemp),(3))
+                
+                ef=maxtemp
+                eg=mintemp
+                xpos+=100
+                x2pos+=100
+            z=z+1
+            
+        
     
     while xgrid<w:
         pygame.draw.line(screen,gray,(0,xgrid),(l,xgrid),(1))
