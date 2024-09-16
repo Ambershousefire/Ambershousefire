a1f5144627598459a03122b4e8ef953ce8000df1
Ambershousefire <amber85077427@gmail.com>
2024-09-16 13:02:58 +0100

    modified:   projects/more tranlater.py

--- projects/more tranlater.py
+++ projects/more tranlater.py
@@ -1,4 +1,19 @@
-text=[]
-text=str(input("what text do you whant to translate: "))
-print(text)
-text.   
+string = input("what to translate: ")
+letter = [x for x in string]
+i=0
+x=0
+y=0
+trans=[]
+morse=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"," ","..--..","-.-.--"]
+lexacon=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"," ","?","!"]
+while i< len(letter):
+    while not letter[y]==lexacon[x]:
+        x+=1
+        if x>=len(lexacon):
+            x=0
+    if letter[y]==lexacon[x]:
+        trans.append(morse[x])
+        i+=1
+        y+=1
+print(trans)
+        
