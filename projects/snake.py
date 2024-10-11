1ff303e3346248e838b159456138e7578e292db4
Ambershousefire <amber85077427@gmail.com>
2024-10-11 13:29:00 +0100

    modified:   projects/snake.py

--- projects/snake.py
+++ projects/snake.py
@@ -10,7 +10,7 @@ appley=0
 xDir=0
 yDir=0
 score=0
-scorecount=0#to check if socre has incressed
+scorecount=0#to check if score has incressed
 running=True
 lines=False
 i=0
@@ -24,18 +24,19 @@ def sectDrawing(xpos, ypos, score, screen, iter):
     else:
         colour = black
     if score>=iter:
-        pygame.draw.circle(screen,colour,(xpos[15*-iter],ypos[15*-iter]),10)#uses the 15th postion in the list multpleyed by iteeration to draw tail
+        pygame.draw.circle(screen,colour,(xpos[15*-iter],ypos[15*-iter]),10)#uses the 15th postion in the list multpleyed by iteration to draw tailing pieces
         #xpos[15*-iter] this only works becuse all postions of players are stored in list
 while running:
     screen.fill((25,200 ,25))
     pygame.time.delay(5)
-    if (x-applex)<15 and (x-applex)>-15 and (y-appley)<15 and (y-appley)>-15:#proxicmaty pickup for apple
+    if (x-applex)<15 and (x-applex)>-15 and (y-appley)<15 and (y-appley)>-15:#proximity detection pickup for apple
         score+=1
-    if score==scorecount:#genrates random postions for apple
+    if score==scorecount:#generates random postions for apple
         applex=r.randint(10,990)
         appley=r.randint(10,490)
         scorecount= score+1
-    if x>1000:#wrap around for player
+        #Wrap around for the player
+    if x>1000:
         x=0
     if x<0:
         x=1000
@@ -46,7 +47,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              running = False
-        if event.type == pygame.KEYDOWN:#contols 
+        if event.type == pygame.KEYDOWN:#controls 
             if event.key == pygame.K_UP or event.key == pygame.K_w:
                 yDir = -1
                 xDir = 0
@@ -60,7 +61,7 @@ while running:
                 xDir = 1
                 yDir = 0 
             if event.key == pygame.K_ESCAPE:
-                score+=1
+                score += 1
             if event.key == pygame.K_e:
                 if lines:
                     lines=False
@@ -68,18 +69,19 @@ while running:
                     lines=True
     x = x +(1*xDir)#controls direction on x 
     y = y + (1*yDir)#controls diretion on y
-    if lines:#drwas a triangle to apple 
+
+    if lines:#draws a triangle to apple 
         pygame.draw.polygon(screen,(255,25,25),((applex,appley),(applex,y),(x,y)),1)
     pygame.draw.circle(screen,(255,25,25),(applex,appley),(5))#draws the apple
     pygame.draw.circle(screen,(0,0,80),(x,y),10)#draws the players
-    xpos.append(x)#adds palyer psotion into list 
-    ypos.append(y)#so that next segment can have a postion
-    while i<score:#draws a number of segemnts baced off of score
+    xpos.append(x)#adds player position into list 
+    ypos.append(y)#so that next segment can have a position
+    while i<score:#draws a number of segments baced off of score
         i+=1
         sectDrawing(xpos, ypos, score, screen, i)
         if ((x<=xpos[i*-15]+5 and x>=xpos[i*-15]-5) and (y<=ypos[i*-15]+5 and y>=ypos[i*-15]-5)) or (( x<=xpos[i*-15+15]+5 and x>=xpos[i*-15+15]-5) and (y<=ypos[i*-15+15]+5 and y>=ypos[i*-15+15]-5)):
             running=False
-    i=0#sets i to zero for next run
+    i=0#sets i to zero for next loop
     if score==100:
         running=False
         print(score,"wow that took forever")
