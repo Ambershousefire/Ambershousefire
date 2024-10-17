25e0135994727cfa518672eb8c2d6ececb3577ca
Ambershousefire <amber85077427@gmail.com>
2024-10-17 14:31:49 +0100

    modified:   projects/fractles/fracle2.py

--- projects/fractles/fracle2.py
+++ projects/fractles/fracle2.py
@@ -1,5 +1,5 @@
 import pygame, random
-l=600
+l=700
 screen=pygame.display.set_mode((l,l))
 x=0
 y=500
@@ -7,8 +7,8 @@ x2=l
 y2=100
 x3=0
 i=0
-black=(0,0,255)
-white=(255,0,0)
+black=(255,0,0)
+white=(0,0,255)
 colour=True
 color=black
 e=0
@@ -17,7 +17,7 @@ e2=l
 key=True
 h=100
 running=True
-screen.fill((0,0,0))
+screen.fill((255,255,255))
 while running:
     if key:
         pygame.draw.lines(screen,color,False,((x,l),(x+100,y),(x+100,l)))
@@ -47,8 +47,28 @@ while running:
             color=(white)
             colour=True
     if not key:
-        pygame.time.delay(5)
-        pygame.draw.lines(screen,color,True,((0,e),(e,l),(l,e2),(e2,0)),(3))
+        pygame.time.delay(50)
+        pygame.draw.lines(screen,color,False,((x,l),(x+100,y),(x+100,l)))
+        pygame.draw.lines(screen,color,False,((x2,l),(x2-100,y2),(x2-100,l)))
+        pygame.draw.lines(screen,color,False,((l,x2),(y2,x2-100),(l,x2-100)))
+        pygame.draw.lines(screen,color,False,((l,x),(y,x+100,),(l,x+100)))
+        pygame.draw.lines(screen,color,False,((x2,l-l),(x2-100,y2),(x2-100,l-l)))
+        pygame.draw.lines(screen,color,False,((x,l-l),(x+100,y2),(x+100,l-l)))
+        pygame.draw.lines(screen,color,False,((l-l,x2),(y2 ,x2-100),(l-l,x2-100)))
+        pygame.draw.lines(screen,color,False,((l-l,x),(y,x+100,),(l-l,x+100)))
+        #pygame.draw.lines(screen,color,True,((0,e),(e,l),(l,e2),(e2,0)),(1))
+        x+=100
+        x2-=100
+        y+=h/2
+        y2-=h/2
+        if x>l:
+            x=0
+        if y>l:
+            y=0
+        if x2<0:
+            x2=l
+        if y2<0:
+            y2=l
         if b:
             i=e
             e+=1
@@ -67,8 +87,6 @@ while running:
             b=True
             e=0
             e2=l
-            
-    
     
     pygame.display.flip()
     for event in pygame.event.get():
@@ -84,6 +102,7 @@ while running:
                     y2=l
                     e=0
                     e2=l
+                    screen.fill((0,0,0))
                 else:
                     key=True
-                    screen.fill((0,0,0))
+                    screen.fill((255,255,255))
