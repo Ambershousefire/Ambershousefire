45dc8da6a400defd369b646fdd26488bf8cdfd83
Ambershousefire <amber85077427@gmail.com>
2024-10-19 19:34:35 +0100

    new file:   projects/colour.py

---
+++ projects/colour.py
@@ -0,0 +1,27 @@
+
+import pygame
+
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+orange = (255,125,0)
+yellow = (255,255,0)
+lime = (128,255,0)
+green = (0,255,0)
+cyan = (0,255,128)
+light_blue = (0,255,255)
+blue = (0,0,255)
+violet = (127,0,255)
+purple = (255,0,255)
+pink = (255,153,204)
+gray = (125,125,125)
+brown = (51,24,0)
+
+def mouse_pos_local(i,z):
+    mousex,mousey=pygame.mouse.get_pos()
+    i=int((mousex/100))
+    z=int((mousey/100))
+    return i,z
+#pygame.draw.polygon(screen,hid[int(b[z]-a[i])],((i*100,z*100),(i*100,z*100+100),(i*100+100,z*100+100),(i*100+100,z*100)))
+#pygame.draw.polygon(screen,hid[int(a[i]-b[z])],((i*100,z*100),(i*100,z*100+100),(i*100+100,z*100+100),(i*100+100,z*100)))
