0812cc125d1d4a6c3954c8398d4e4d5e18100017
Ambershousefire <amber85077427@gmail.com>
2024-10-19 19:41:53 +0100

    modified:   projects/colour.py

--- projects/colour.py
+++ projects/colour.py
@@ -1,22 +1,22 @@
 
 import pygame
 #colour
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-orange = (255,125,0)
-yellow = (255,255,0)
-lime = (128,255,0)
-green = (0,255,0)
-cyan = (0,255,128)
-light_blue = (0,255,255)
-blue = (0,0,255)
-violet = (127,0,255)
-purple = (255,0,255)
-pink = (255,153,204)
-gray = (125,125,125)
-brown = (51,24,0)
-
+black = (0,0,0)#1
+white = (255,255,255)#2
+red = (255,0,0)#3
+orange = (255,125,0)#4
+yellow = (255,255,0)#5
+lime = (128,255,0)#6
+green = (0,255,0)#7
+cyan = (0,255,128)#8
+light_blue = (0,255,255)#9
+blue = (0,0,255)#10
+violet = (127,0,255)#11
+purple = (255,0,255)#12
+pink = (255,153,204)#13
+gray = (125,125,125)#14
+brown = (51,24,0)#15
+colour_list = [black,white,red,orange,yellow,lime,green,cyan,light_blue,blue,violet,purple,pink,gray,brown]
 def mouse_pos_local(w):
     i=0
     z=0
