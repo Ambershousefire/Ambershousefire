950fbfed06c0322501e2ef82171672b055535318
Ambershousefire <amber85077427@gmail.com>
2024-11-05 10:34:09 +0000

    modified:   .gitignore modified:   projects/games/rockbattle.py

--- projects/games/rockbattle.py
+++ projects/games/rockbattle.py
@@ -1,5 +1,7 @@
 
-import random
+import random , os
+
+file = open("projects\\games\\win_rate.txt","a")
 win= "  You Won :3"
 loss="  You lost >:3"
 draw="  Thats a Draw"
@@ -9,54 +11,61 @@ run=input("do you whant to play rock paper sisors(yes or no): ")
 if run.capitalize().startswith("Y"):
     run=True
 elif not run.capitalize().startswith("Y"):
-    
+
         print("aww i whanted to play")
-        
+
 if run==True:
     npc=[1,1,1,2,2,2,3,3,3] #rock is 1 paper is 2 paper 3 is sissiors
     b=0
-    
+
     while runtime:
         print("wins",w,"losses",l,"draws",d)
-        
+
         b=b+1
-        
+
         x=int(input("ok to paly rock(1) paper(2) sisors(3) you will say what you whant to play and we will see who wins: "))    
-        z=npc[random.randint(0,2)]
-        
+        z=npc[random.randint(0,8)]
+
         if z==x:
             d+=1
             print(draw)
-        
+
         elif z==(1):#R
             if x==2:#P
                 w+=1
                 print (win)
-            
+
         elif x==3:#S
             l+=1
-            print (loss)  
-                
+            print (loss)
+
         elif z==(2):#P
             if x==3:#S
                 w+= 1
                 print (win)
-            
+
         elif x==1:#R
             l+= 1
             print(loss)
-                
+
         elif z==(3):#S
             if x==1:#R
                 w+=1
                 print(win)
-            
+
         elif x==2:#P
             l+=1
             print(loss)
-            
+
         if w == 3:
             runtime = False
         elif l == 3:
             runtime = False
     print("wins",w,"losses",l,"draws",d)
+    file.write("\nrock battle: ")
+    file.write("wins: ")
+    file.write(str(w))
+    file.write(" losses: ")
+    file.write(str(l))
+    file.write(" draws: ")
+    file.write(str(d))
