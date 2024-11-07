a892629961de04fe78ef6ecd38c852c076f18ce0
Ambershousefire <amber85077427@gmail.com>
2024-11-07 23:13:56 +0000

    modified:   projects/for_text.py

--- projects/for_text.py
+++ projects/for_text.py
@@ -1,3 +1,13 @@
+#test=["0","1","2","3","4","5","6","7","8","9"]
+#deck =[x+y+z for x in test for y in test for z in test]
+
+#[print (x) for x in deck]
+#print(len(deck))
+#test2 = [i for i in range(10) if i%2 == 0]
+#print(test2)
+#test3=["3","7.4","8.2"]
+#test3=[float(x) for x in test3]
+#print(test3)
 
 x=1
 z=1
