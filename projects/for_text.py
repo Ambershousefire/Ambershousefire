cef06060e678c776c252e7f87f9a9f81a8aaa38d
Ambershousefire <amber85077427@gmail.com>
2024-11-07 14:33:29 +0000

    modified:   projects/for_text.py

--- projects/for_text.py
+++ projects/for_text.py
@@ -1,7 +1,13 @@
 
-test=["1","2","3"]
-test2=["a","b","c"]
-suit = ["h", "s"]
-deck =[x+y+z for x in test for y in suit for z in test2]
+test=["0","1","2","3","4","5","6","7","8","9"]
+deck =[x+y+z for x in test for y in test for z in test]
 
-[print (x) for x in deck]
+#[print (x) for x in deck]
+print(len(deck))
+test2 = [i for i in range(10) if i%2 == 0]
+print(test2)
+test3=["3","7.4","8.2"]
+test3=[float(x) for x in test3]
+print(test3)
+
+#https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
