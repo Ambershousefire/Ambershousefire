527bba7ba636d1c98ab2ae3ea57ad1bc5e210a62
Ambershousefire <amber85077427@gmail.com>
2024-12-02 12:44:36 +0000

    new file:   advent/advent day one.py new file:   advent/advent.txt new file:   advent/advent_2.py new file:   advent/avdent_2.txt

---
+++ advent/advent_2.py
@@ -0,0 +1,24 @@
+file = open("advent/avdent_2.txt","r").readlines(-1)
+file=[x for x in file]
+x=0
+i=1
+count=1
+zet=0
+cet=0
+while not x==1000:
+    cut=file[x].split(" ")
+    while not (i-2)==len(cut):
+        try:
+            if (int(cut[i])-int(cut[i+1]))==( -1 or -2):
+                zet+=1
+            elif (int(cut[i])-int(cut[i+1]))==(1 or 2 or 3):
+                zet-=1
+        except:
+            zet+=0
+        i+=1
+        if zet==5 or zet==-5:
+            cet+=1
+    x+=1
+    i=0
+    zet=0
+print(cet)
