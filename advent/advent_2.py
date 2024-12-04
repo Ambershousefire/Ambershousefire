3a0765c67314bb2fda8e98d484fcbf4d87d909a6
Ambershousefire <amber85077427@gmail.com>
2024-12-04 10:45:45 +0000

    renamed:    advent/advent day one.py -> advent/advent_1.py renamed:    advent/advent.txt -> advent/advent_1.txt modified:   advent/advent_2.py new file:   advent/advent_2.txt new file:   advent/advent_3.py new file:   advent/advent_3.txt deleted:    advent/avdent_2.txt

--- advent/advent_2.py
+++ advent/advent_2.py
@@ -1,4 +1,4 @@
-file = open("advent/avdent_2.txt","r").readlines(-1)
+file = open("advent/advent_2.txt","r").readlines(-1)
 file=[x for x in file]
 x=0
 i=0
@@ -8,34 +8,37 @@ zet=[]
 while not x==len(file):
     cut=file[x].split(" ")
 
-    if (sorted(cut)==cut) or (sorted(cut,reverse=True)==cut):
+    if (sorted(cut,reverse=False)==cut) or (sorted(cut,reverse=True)==cut):
         zet.append(cut)
     x+=1
 x=0
 z=0
 y=0
-i=1
+i=0
 f=1
 get=0
 while not z==len(zet):
     for x in zet[z]:
         but.append(int(x))
     while not f == len(but):
-        print(i)
-        if int(but[f])-int(but[f])==(-1 or -2 or -3):
+        
+        
+        if (int(but[f-1])-int(but[f])==1) or (int(but[f-1])-int(but[f])==2) or (int(but[f-1])-int(but[f])==3) :
             i+=1
-        elif but[f-1]==but[f]:
-            print("same")
+            print("done")
+        else:
+            ("not")
         f+=1
-        if i==(len(but)):
+        if i==(len(but)-1):
             y+=1
-        elif i==-(len(but)):
-            y+=1
+        print(i)
+        print(but)
     print(len(but),"count")
 
-    i=1
+    i=0
     z+=1
     f=0
     but=[]
     print("\n")
 print(y)
+print(len(zet))
