ed22bf6a7f46f4eda41d15cf5c2af2eff014d6a6
Ambershousefire <amber85077427@gmail.com>
2024-12-05 14:41:38 +0000

    message:i hate this code modified:   advent/advent_2.py

--- advent/advent_2.py
+++ advent/advent_2.py
@@ -12,7 +12,7 @@ while not x ==-1:
     x+=1
 x=0
 #len of lsit 1k
-#list_1.sort()
+
 while not x == -1:
     try:
         list_1[x]=str(list_1[x]).split(" ")
@@ -20,38 +20,54 @@ while not x == -1:
         break
     x+=1
 x=0
-z=0
 i=0
 while not x == 1000:
     try:
-        if (0<(int(list_1[x][z+0])-int(list_1[x][z+1])<4) and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4) and 0<(int(list_1[x][z+2])-int(list_1[x][z+3])<4) and 0<(int(list_1[x][z+3])-int(list_1[x][z+4])<4)) and 0<(int(list_1[x][z+4])-int(list_1[x][z+5])<4 and  0<(int(list_1[x][z+5])-int(list_1[x][z+6])<4) and 0<(int(list_1[x][z+6])-int(list_1[x][z+7])<4) and 0<(int(list_1[x][z+7])-int(list_1[x][z+8])<4)):
+        if (0<(int(list_1[x][0])-int(list_1[x][1])<4) and 0<(int(list_1[x][1])-int(list_1[x][2])<4) and 0<(int(list_1[x][2])-int(list_1[x][3])<4) and 0<(int(list_1[x][3])-int(list_1[x][4])<4)) and 0<(int(list_1[x][4])-int(list_1[x][5])<4 and  0<(int(list_1[x][5])-int(list_1[x][6])<4) and 0<(int(list_1[x][6])-int(list_1[x][7])<4) and 0<(int(list_1[x][7])-int(list_1[x][8])<4)):
             i+=1
     except:
         try:
-            if (0<(int(list_1[x][z+0])-int(list_1[x][z+1])<4) and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4) and 0<(int(list_1[x][z+2])-int(list_1[x][z+3])<4) and 0<(int(list_1[x][z+3])-int(list_1[x][z+4])<4)) and 0<int(list_1[x][z+4])-int(list_1[x][z+5])<4 and  0<(int(list_1[x][z+5])-int(list_1[x][z+6])<4) and 0<(int(list_1[x][z+6])-int(list_1[x][z+7])<4):
+            if (0<(int(list_1[x][0])-int(list_1[x][1])<4) and 0<(int(list_1[x][1])-int(list_1[x][2])<4) and 0<(int(list_1[x][2])-int(list_1[x][3])<4) and 0<(int(list_1[x][3])-int(list_1[x][4])<4)) and 0<int(list_1[x][4])-int(list_1[x][5])<4 and  0<(int(list_1[x][5])-int(list_1[x][6])<4) and 0<(int(list_1[x][6])-int(list_1[x][7])<4):
                 i+=1
         except:
             try:
-                if (0<(int(list_1[x][z+0])-int(list_1[x][z+1])<4) and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4) and 0<(int(list_1[x][z+2])-int(list_1[x][z+3])<4) and 0<(int(list_1[x][z+3])-int(list_1[x][z+4])<4)) and 0<int(list_1[x][z+4])-int(list_1[x][z+5])<4 and  0<int(list_1[x][z+5])-int(list_1[x][z+6])<4:
+                if (0<(int(list_1[x][0])-int(list_1[x][1])<4) and 0<(int(list_1[x][1])-int(list_1[x][2])<4) and 0<(int(list_1[x][2])-int(list_1[x][3])<4) and 0<(int(list_1[x][3])-int(list_1[x][4])<4)) and 0<int(list_1[x][4])-int(list_1[x][5])<4 and  0<int(list_1[x][5])-int(list_1[x][6])<4:
                     i+=1
             except:
                 try:
-                    if (0<(int(list_1[x][z+0])-int(list_1[x][z+1])<4) and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4) and 0<(int(list_1[x][z+2])-int(list_1[x][z+3])<4) and 0<(int(list_1[x][z+3])-int(list_1[x][z+4])<4)) and 0<int(list_1[x][z+4])-int(list_1[x][z+5])<4:
+                    if (0<(int(list_1[x][0])-int(list_1[x][1])<4) and 0<(int(list_1[x][1])-int(list_1[x][2])<4) and 0<(int(list_1[x][2])-int(list_1[x][3])<4) and 0<(int(list_1[x][3])-int(list_1[x][4])<4)) and 0<int(list_1[x][4])-int(list_1[x][5])<4:
                         i+=1
                 except:
                     try:
-                        if (0<(int(list_1[x][z+0])-int(list_1[x][z+1])<4) and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4) and 0<(int(list_1[x][z+2])-int(list_1[x][z+3])<4) and 0<(int(list_1[x][z+3])-int(list_1[x][z+4])<4)):
+                        if (0<(int(list_1[x][0])-int(list_1[x][1])<4) and 0<(int(list_1[x][1])-int(list_1[x][2])<4) and 0<(int(list_1[x][2])-int(list_1[x][3])<4) and 0<(int(list_1[x][3])-int(list_1[x][4])<4)):
                             i+=1
                     except:
-                        try:
-                            if 0<(int(list_1[x][z+0])-int(list_1[x][z+1])<4) and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4) and 0<(int(list_1[x][z+2])-int(list_1[x][z+3])<4):
-                                i+=1
-                        except:
-                            try:
-                                if (0<int(list_1[x][z+0])-int(list_1[x][z+1]))<4 and 0<(int(list_1[x][z+1])-int(list_1[x][z+2])<4):
-                                    i+=1
-                            except:
-                                pass
+                        pass
+    finally:
+        x+=1
+x=0
+while not x == 1000:
+    try:
+        if (-4<(int(list_1[x][0])-int(list_1[x][1])<0) and -4<(int(list_1[x][1])-int(list_1[x][2])<0) and -4<(int(list_1[x][2])-int(list_1[x][3])<0) and -4<(int(list_1[x][3])-int(list_1[x][4])<0)) and -4<(int(list_1[x][4])-int(list_1[x][5])<0 and  -4<(int(list_1[x][5])-int(list_1[x][6])<0) and -4<(int(list_1[x][6])-int(list_1[x][7])<0) and -4<(int(list_1[x][7])-int(list_1[x][8])<0)):
+            i+=1
+    except:
+        try:
+            if (-4<(int(list_1[x][0])-int(list_1[x][1])<0) and -4<(int(list_1[x][1])-int(list_1[x][2])<0) and -4<(int(list_1[x][2])-int(list_1[x][3])<0) and -4<(int(list_1[x][3])-int(list_1[x][4])<0)) and -4<int(list_1[x][4])-int(list_1[x][5])<0 and  -4<(int(list_1[x][5])-int(list_1[x][6])<0) and -4<(int(list_1[x][6])-int(list_1[x][7])<0):
+                i+=1
+        except:
+            try:
+                if (-4<(int(list_1[x][0])-int(list_1[x][1])<0) and -4<(int(list_1[x][1])-int(list_1[x][2])<0) and -4<(int(list_1[x][2])-int(list_1[x][3])<0) and -4<(int(list_1[x][3])-int(list_1[x][4])<0)) and -4<int(list_1[x][4])-int(list_1[x][5])<0 and  -4<int(list_1[x][5])-int(list_1[x][6])<0:
+                    i+=1
+            except:
+                try:
+                    if (-4<(int(list_1[x][0])-int(list_1[x][1])<0) and -4<(int(list_1[x][1])-int(list_1[x][2])<0) and -4<(int(list_1[x][2])-int(list_1[x][3])<0) and -4<(int(list_1[x][3])-int(list_1[x][4])<0)) and -4<int(list_1[x][4])-int(list_1[x][5])<0:
+                        i+=1
+                except:
+                    try:
+                        if (-4<(int(list_1[x][0])-int(list_1[x][1])<0) and -4<(int(list_1[x][1])-int(list_1[x][2])<0) and -4<(int(list_1[x][2])-int(list_1[x][3])<0) and -4<(int(list_1[x][3])-int(list_1[x][4])<0)):
+                            i+=1
+                    except:
+                        pass
     finally:
         x+=1
 print(i)
