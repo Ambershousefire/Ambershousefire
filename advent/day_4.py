1d445d3f1262b5075b48b978fa050339c894f18d
Ambershousefire <amber85077427@gmail.com>
2024-12-06 12:02:21 +0000

    modified:   advent/day_4.py

--- advent/day_4.py
+++ advent/day_4.py
@@ -9,43 +9,31 @@ while not len(full_list)==len(file):
 x=0
 i=0
 xmas=0
-ewxmas=0
-nsxmas=0
-neswxmas=0
-nwsexmas=0
-while not x == 1440:
+while not x == len(full_list[0]):
     try:
-        if (full_list[x][0][i]+full_list[x][0][i+1]+full_list[x][0][i+2]+full_list[x][0][i+3])==("XMAS"or"SAMX"):
-            ewxmas+=1
-            print("test")
+        if (full_list[x][0][i]+full_list[x][0][i+1]+full_list[x][0][i+2]+full_list[x][0][i+3])=="XMAS"or"SAMX":
+            xmas+=1
     except:
         pass
     try:
-        if (full_list[x][0][i]+full_list[x+1][0][i+1]+full_list[x+2][0][i+2]+full_list[x+3][0][i+3])==("XMAS"or"SAMX"):
-            neswxmas+=1
-            #print("test")
+        if (full_list[x][0][i]+full_list[x+1][0][i+1]+full_list[x+2][0][i+2]+full_list[x+3][0][i+3])=="XMAS"or"SAMX":
+            xmas+=1
     except:
         pass
     try: 
-        if (full_list[x][0][i]+full_list[x+1][0][i]+full_list[x+2][0][i]+full_list[x+3][0][i])==("XMAS"or"SAMX"):
-            nsxmas+=1
-            #print("test")
+        if (full_list[x][0][i]+full_list[x+1][0][i]+full_list[x+2][0][i]+full_list[x+3][0][i])=="XMAS"or"SAMX":
+            xmas+=1
     except:
         pass
     try:
-        if (full_list[x][0][i]+full_list[x-1][0][i+1]+full_list[x-2][0][i+2]+full_list[x-3][0][i+3])==("XMAS"or"SAMX"):
-            nwsexmas+=1
-            #print("test")
+        if (full_list[x][0][i]+full_list[x-1][0][i+1]+full_list[x-2][0][i+2]+full_list[x-3][0][i+3])=="XMAS"or"SAMX":
+            xmas+=1
     except:
         pass
-    if i==140:
+    if i==len(full_list[0][0]):
         i=0
         x+=1
     i+=1
 i=0
 x=0
-
-
-print(ewxmas,nsxmas,neswxmas,nwsexmas)
-xmas=ewxmas+nsxmas+neswxmas+nwsexmas
 print(xmas)
